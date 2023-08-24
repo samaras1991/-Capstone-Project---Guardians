20e36d02a5b1e51b57dbcd7f7c75680843682e1b
mohamed <fcmedox@gmail.com>
2023-08-24 10:38:24 -0400

    new change

--- src/test/java/tek/sdet/framework/runner/TestRunner.java
+++ src/test/java/tek/sdet/framework/runner/TestRunner.java
@@ -7,7 +7,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        tags =  "@Regression1",
+        tags =  "@Regression",
         features = ("classpath:features"),
         glue="tek.sdet.framework",
         dryRun = false,
